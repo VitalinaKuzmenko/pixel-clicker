ac17bb5d80dd18c3a555c56ebf4d812fdc502f45
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2024-01-01 12:03:06 +0000

    Create a small Full-stack app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState("");
+
+  const handleClick = async () => {
+    const response = await fetch("http://localhost:5000/message");
+    const data = await response.json();
+    setMessage(data.message);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClick}>Click me</button>
+        <p>{message}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
